2deae8c2c2e9a5a8c47964d8a9282f343c249894
Andrewknowlton17 <122592188+Andrewknowlton17@users.noreply.github.com>
2023-05-01 11:47:51 -0600

    Add files via upload

---
+++ Exam_2/knowltonexam2.R
@@ -0,0 +1,24 @@
+library(tidyverse)
+library(janitor)
+library(modelr)
+df <- read.csv("unicef-u5mr.csv")
+df <- df %>%pivot_longer(-c(CountryName,Continent,Region), names_prefix = "U5MR.", names_transform = as.numeric)
+df <- df %>% clean_names()
+
+df %>% ggplot(aes(x=name,y=value,group=country_name)) + geom_line()+ facet_wrap(~continent)
+
+
+
+df %>% group_by(continent,name) %>%  summarize(u5mr_mean = mean(value,na.rm = TRUE)) %>%  
+  ggplot(aes(x=name, y=u5mr_mean,color=continent))+ geom_line()
+
+
+mod1 <- glm(data = df, formula = value ~ name)
+
+mod2 <-  glm(data=df, formula = value ~ name +continent)
+
+mod3 <-  glm(data= df , formula = value~ name * continent )
+
+gather_predictions(df,mod1,mod2,mod3) %>%  ggplot(aes(x=name,y=pred,color = continent))+ 
+  geom_smooth(method= "glm") + facet_wrap(~model)
+
